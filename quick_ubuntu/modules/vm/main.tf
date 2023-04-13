045993f2d612abe72256f3ab0040c86e87d4fe43
mycielski <mycielski00@gmail.com>
2023-04-13 11:35:27 +0200

    refactor: extract security group rules to separate resources

--- quick_ubuntu/modules/vm/main.tf
+++ quick_ubuntu/modules/vm/main.tf
@@ -17,21 +17,25 @@ resource "aws_security_group" "default" {
   name        = "terraform-sg"
   description = "Allow SSH from anywhere and all outbound traffic"
   vpc_id      = var.vpc_id
+}
 
-  ingress {
-    description = "SSH"
-    to_port     = 22
-    from_port   = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  description       = "SSH from the world"
+  from_port         = 22 
+  to_port           = 22 
+  protocol          = "tcp" 
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default.id
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0 
+  to_port           = 0 
+  protocol          = "-1" 
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default.id
 }
 
 data "aws_ami" "ubuntu" {
@@ -39,7 +43,7 @@ data "aws_ami" "ubuntu" {
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-2*.04-*-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-*-*-server-*"]
   }
 }
 
